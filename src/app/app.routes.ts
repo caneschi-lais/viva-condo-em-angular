bc34b54853b3a841b73cce0fbe817f780e4f0ce0
Laís Caneschi de Siqueira <lais.caneschi.siqueira@gmail.com>
2025-12-01 23:55:46 -0300

    Atualizar o app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { CondominiosComponent } from './pages/condominios/condominios.component';
 import { UsuariosComponent } from './pages/usuarios/usuarios.component';
-import { authGuard } from '../guards/auth.guard'; // Importe o guard
+import { authGuard } from '../guards/auth.guard';
 
 export const routes: Routes = [
   { 
