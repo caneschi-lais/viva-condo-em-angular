4de0e43d104b2008132b4cba852eefa1e39e0b64
Laís Caneschi de Siqueira <lais.caneschi.siqueira@gmail.com>
2025-12-01 23:54:49 -0300

    Atualizar o app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,14 +2,12 @@ import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListen
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
-// Importe o módulo e os ícones que você usa
 import { LucideAngularModule, Building2, Users, LogOut, Plus, Pencil, Trash2 } from 'lucide-angular';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    // Adicione esta linha para registrar os ícones globalmente
-    importProvidersFrom(LucideAngularModule.pick({ Building2, Users, LogOut, Plus, Pencil, Trash2 }))
+     importProvidersFrom(LucideAngularModule.pick({ Building2, Users, LogOut, Plus, Pencil, Trash2 }))
   ]
 };
